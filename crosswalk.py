c25ef2321dcdba0bd3d7966bb4cd69f8595872f9
JedMenard <jmenard@mymail.mines.edu>
2017-11-20 16:59:34 -0700

    Initial commit

---
+++ crosswalk.py
@@ -0,0 +1,73 @@
+from events import *
+import queue as Q
+import math
+
+
+def uniformToExponential(u, l):     # u is uniform, l is associated lambda
+    e = -math.log(u)/l              # called l because lambda is reseved in python
+    return e
+
+def getTimes(filename):
+    times = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            times.append(float(line.strip()))
+    return times
+
+def processEvent(e):
+    # This is going to be where the bulk of the work is contained.
+    # I'm thinking we have this check what type of event e is
+    # and call the associated function for proccessing that
+    # particular event.
+    # For clarity sake, I have defined these helper
+    # functions in a separate file
+    
+    if (e == 'pedSpawn'):
+        pedSpawn()
+    elif (e == 'pedArrival'):
+        pedArrival()
+    elif (e == 'pedExit'):
+        pedExit()
+    elif (e == 'buttonPress'):
+        buttonPress()
+    elif (e == 'autoSpawn'):
+        autoSpawn()
+    elif (e == 'autoArrival'):
+        autoArrival()
+    elif (e == 'autoExit'):
+        autoExit()
+    elif (e == 'redExpires'):
+        redExpires()
+    elif (e == 'yellowExpires'):
+        yellowExpires()
+    elif (e == 'greenExpires'):
+        greenExpires()
+    elif (e == 'startWalk'):
+        startWalk()
+    elif (e == 'endWalk'):
+        endWalk()
+    return
+
+def main(N, randomAuto, randomPed, randomButtons):
+    B = 330     # Width of a residential block
+    w = 24      # Width of crosswalk
+    S = 46      # Width of street 
+    RED = 18    # Time traffic signal is red
+    YELLOW = 8  # Time traffic signal is yellow
+    GREEN = 35  # Minimum time traffic signal is green
+    rp = 3      # Arrival rate of pedestrians (per minute)
+    ra = 4      # Arrival rate of automobiles (per minute)
+    L = 9       # Length of an automobile
+                # vj: auto speed, Uniform(25,35) mph
+                # a: auto acceleration, 10 ft/s/s
+                # vk: ped speek, uniform(2.6, 4.1) ft/s
+    
+
+    # Create arrays of uniform distributions
+    autoTimes = getTimes(randomAuto)
+    pedTimes = getTimes(randomPed)
+    buttonTimes = getTimes(randomButtons)
+
+    # Define system state variables
+    light = 'g'
+    eventList = Q.PriorityQueue     # eventList is now a priority queue
---
+++ events.py
@@ -0,0 +1,48 @@
+def pedSpawn():
+    # A pedestrian is spawned in the system (a block away from the crosswalk)
+    return
+
+def pedArrival():
+    # A pedestrian arrives at the crosswalk
+    return
+
+def pedExit():
+    # A pedestrian exits the system
+    return
+
+def buttonPress():
+    # A pedestrian pushes a button
+    return
+
+def autoSpawn():
+    # An automobile is spawned in the system
+    return
+
+def autoArrival():
+    # An automobile arives at the intersection
+    return
+
+def autoExit():
+    # An automobile exits the system
+    return
+
+def redExpires():
+    # Light changes from red to green
+    return
+
+def yellowExpires():
+    # Light changes from yellow to red
+    return
+
+def greenExpires():
+    # Light changes from green to yellow
+    return
+
+def startWalk():
+    # Crosswalk sign turns on, peds start to cross
+    return
+
+def endWalk():
+    # Crosswalk sign turns off, no more crossing
+    return
+
