4ac7ae667195206e625132eab56d197d095096c2
JedMenard <Jmenard@mymail.mines.edu>
2017-11-20 20:13:51 -0700

    Making some progress...

--- crosswalk.py
+++ crosswalk.py
@@ -2,17 +2,97 @@ from events import *
 import Queue as Q
 import math
 
-# Defining some global variables
+# Defining some global system variables
 # This is to prevent function calls with a million parameters and return values
-pedDelays = []
-autoDelays = []
-pedsInSystem = []       # Needs to be treated as a queue, but also needs to have indexed-based access
-autosInSystem = []      # See above comment
-autoTimes = []
-pedTimes = []
+time = 0
 buttonTimes = []
+pedTimes = []
+autoTimes = []
+autosInSystem = []          # Needs to be treated as a queue, but also needs to have indexed-based access
+pedsInSystem = []           # See above comment
+pedsWaiting = []
+autosWaiting = []
+autoDelays = []
+pedDelays = []
 eventList = Q.PriorityQueue()
-time = 0.   # Global system time
+walkLight = 'red'
+driveLight = 'green'
+lastLightChange = 0
+
+
+# Defining some global constants
+B = 330     # Width of a residential block
+w = 24      # Width of crosswalk
+S = 46      # Width of street 
+RED = 18    # Time traffic signal is red
+YELLOW = 8  # Time traffic signal is yellow
+GREEN = 35  # Minimum time traffic signal is green
+rp = 3./60   # Arrival rate of pedestrians (per second)
+ra = 4./60   # Arrival rate of automobiles (per second)
+L = 9       # Length of an automobile
+            # vj: auto speed, Uniform(25,35) mph = Uniform(25,35)/360 miles/second
+            # a: auto acceleration, 10 ft/s/s
+            # vk: ped speek, uniform(2.6, 4.1) ft/s
+
+def main(N, randomAuto, randomPed, randomButtons):
+    global time
+    global eventList
+    global autoTimes
+    global pedTimes
+    global buttonTimes
+
+    # Create arrays of uniform distributions
+    autoTimes = getTimes(randomAuto)
+    pedTimes = getTimes(randomPed)
+    buttonTimes = getTimes(randomButtons)
+
+    # Define system state variables
+    eventCounter = 0;
+
+    # Define initial event times
+    nextPedTime = time + uniformToExponential(pedTimes.pop(), rp)
+    nextAutoTime = time + uniformToExponential(autoTimes.pop(), ra)
+    nextLightChange = time
+    nextButtonPress = time
+
+    # Define initial events
+    nextPed = (nextPedTime, 'pedSpawn')
+    nextAuto = (nextAutoTime, 'autoSpawn')
+    nextLight = (nextLightChange, 'greenExpires')
+    nextButton = (nextButtonPress, 'buttonPress')
+
+    # Enqueue initial events
+    eventList.put(nextPed)
+    #eventList.put(nextAuto)
+    eventList.put(nextLight)
+    #eventList.put(nextButton)
+    #eventList.put((120, 'startWalk'))
+
+    while (eventCounter < N):
+        event = eventList.get()
+        time = event[0]
+        print "~~~~~~~~~~Next event~~~~~~~~~~"
+        print "Event type: {}".format(event[1])
+        print "Events to date: {}".format(eventCounter)
+        print "Time: {}".format(event[0])
+        print "Stoplight color: {}".format(driveLight)
+        print "Crosswalk color: {}".format(walkLight)
+        print "Number of pedestrains in system: {}".format(len(pedsInSystem))
+        print "Number of pedestrains waiting: {}".format(len(pedsWaiting))
+        print "Number of pedestrains crossed: {}".format(len(pedDelays))
+        print "Number of automobiles in system: {}".format(len(autosInSystem))
+        print "Number of automobiles waiting: {}".format(len(autosWaiting))
+        print "Number of automobiles crossed: {}".format(len(autoDelays))
+        print
+        
+        processEvent(event, N)
+        eventCounter += 1
+        raw_input()
+
+        if (eventList.empty()):
+            break
+
+
 
 def uniformToExponential(u, l):     # u is uniform, l is associated lambda
     e = -math.log(u)/l              # called l because lambda is reseved in python
@@ -25,95 +105,71 @@ def getTimes(filename):
             times.append(float(line.strip()))
     return times
 
-def processEvent(e):
+def processEvent(event, N):
     # This is going to be where the bulk of the work is contained.
     # I'm thinking we have this check what type of event e is
     # and call the associated function for proccessing that
     # particular event.
     # For clarity sake, I have defined these helper
     # functions in a separate file
+
+    # Because python is weird, need to specify that eventList is global
+    global eventList
+    global pedsInSystem, pedsWaiting, pedDelays
+    global autosInSystem, autosWaiting, autoDelays
+    global walkLight, lastLightChange, driveLight
+
+    e = event[1]
     
     if (e == 'pedSpawn'):
-        pedSpawn()
+        N += 1
+        speed = pedTimes.pop()*(4.1-2.6) + 2.6
+        pedsInSystem = pedSpawn(eventList, pedsInSystem, time, speed, B + S)
+        
     elif (e == 'pedArrival'):
-        pedArrival()
+        pedsInSystem, pedsWaiting, eventList = pedArrival(time, eventList, pedsInSystem, pedsWaiting, walkLight, lastLightChange)
+        
     elif (e == 'pedExit'):
-        pedExit()
+        ped = event[2]
+        pedDelays = pedExit(time, pedDelays, ped)
+        
     elif (e == 'buttonPress'):
         buttonPress()
+        
     elif (e == 'autoSpawn'):
-        autoSpawn()
+        N += 1
+        autosInSystem = autoSpawn(autosInSystem, time)
+        
     elif (e == 'autoArrival'):
         autoArrival()
+        
     elif (e == 'autoExit'):
         autoExit()
+        
     elif (e == 'redExpires'):
-        redExpires()
+        lastLightChange = time
+        driveLight = 'green'
+        eventList = redExpires(eventList, GREEN, time)
+        
     elif (e == 'yellowExpires'):
-        yellowExpires()
+        lastLightChange = time
+        driveLight = 'red'
+        eventList = yellowExpires(eventList, RED, time)
+        
     elif (e == 'greenExpires'):
-        greenExpires()
+        lastLightChange = time
+        driveLight = 'yellow'
+        eventList = greenExpires(eventList, YELLOW, time)
+        
     elif (e == 'startWalk'):
-        startWalk()
+        walkLight = 'green'
+        pedsWaiting, eventList = startWalk(time, pedsWaiting, eventList)
+        
     elif (e == 'endWalk'):
-        endWalk()
+        walkLight = 'red'
+        endWalk(pedsInSystem)
+        
     return
-
-def main(N, randomAuto, randomPed, randomButtons):
-    B = 330     # Width of a residential block
-    w = 24      # Width of crosswalk
-    S = 46      # Width of street 
-    RED = 18    # Time traffic signal is red
-    YELLOW = 8  # Time traffic signal is yellow
-    GREEN = 35  # Minimum time traffic signal is green
-    rp = 3      # Arrival rate of pedestrians (per minute)
-    ra = 4      # Arrival rate of automobiles (per minute)
-    L = 9       # Length of an automobile
-                # vj: auto speed, Uniform(25,35) mph
-                # a: auto acceleration, 10 ft/s/s
-                # vk: ped speek, uniform(2.6, 4.1) ft/s
-
-    # Create arrays of uniform distributions
-    autoTimes = getTimes(randomAuto)
-    pedTimes = getTimes(randomPed)
-    buttonTimes = getTimes(randomButtons)
-
-    # Define system state variables
-    light = 'g'
-    eventCounter = 0;
-
-    # Define initial event times
-    nextPedTime = time + uniformToExponential(pedTimes.pop(), rp)
-    nextAutoTime = time + uniformToExponential(autoTimes.pop(), ra)
-    nextLightChange = time
-    nextButtonPress = time
-
-    # Define initial events
-    nextPed = (nextPedTime, 'pedArrival')
-    nextAuto = (nextAutoTime, 'autoArrival')
-    nextLight = (nextLightChange, 'greenExpires')
-    nextButton = (nextButtonPress, 'buttonPress')
-
-    # Enqueue initial events
-    # TODO: Uncomment when it's time to work on peds
-    #eventList.put(nextPed)
-    # TODO: Uncomment when it's time to work on autos
-    #eventList.put(nextAuto)
-    eventList.put(nextLight)
-    # TODO: Unocmment when it's time to work on autos
-    #eventList.put(nextButton)
-
-    while (eventCounter < N):
-        e = eventList.get()
-        print e
-        processEvent(e)
-        eventCounter += 1
-        raw_input()
-        break
-
-
-
-
 
 
 
@@ -178,4 +234,4 @@ def main(N, randomAuto, randomPed, randomButtons):
 
     
 if __name__ == '__main__':
-    main(100, 'uniform-0-1-00.dat', 'uniform-0-1-01.dat', 'uniform-0-1-02.dat')
+    main(20, 'uniform-0-1-00.dat', 'uniform-0-1-01.dat', 'uniform-0-1-02.dat')
--- events.py
+++ events.py
@@ -1,21 +1,43 @@
-def pedSpawn():
+def pedSpawn(eventList, pedsInSystem, time, speed, distance):
     # A pedestrian is spawned in the system (a block away from the crosswalk)
-    return
+    # TODO: Add ped to system
+    ped = (time, speed)
+    pedsInSystem.append(ped)
 
-def pedArrival():
+    nextArrivalTime = distance/speed
+    nextArrival = (time + nextArrivalTime, 'pedArrival')
+    eventList.put(nextArrival)
+    return pedsInSystem
+
+def pedArrival(time, eventList, pedsInSystem, pedsWaiting, light, lastLightChange):
     # A pedestrian arrives at the crosswalk
-    return
+    ped = pedsInSystem.pop(0)
+    if light == 'green':
+        crosstime = 46/ped[1]
+        if crosstime < 18 - (time - lastLightChange):
+            exitTime = time + crosstime
+            exitEvent = (exitTime, 'pedExit', ped)
+            eventList.put(exitEvent)
+    else:
+        pedsWaiting.append(ped)
+        
+    return pedsInSystem, pedsWaiting, eventList
 
-def pedExit():
+def pedExit(time, pedDelays, ped):
     # A pedestrian exits the system
-    return
+    expectedTime = (330+46*2)/ped[1]
+    delay = (time - ped[0]) - expectedTime
+    pedDelays.append(delay)
+    return pedDelays
 
 def buttonPress():
     # A pedestrian pushes a button
     return
 
-def autoSpawn():
+def autoSpawn(eventList, autosInSystem, autoTimes, time):
     # An automobile is spawned in the system
+    #TODO: Add auto to system
+    autosInSystem.append(time)
     return
 
 def autoArrival():
@@ -26,23 +48,98 @@ def autoExit():
     # An automobile exits the system
     return
 
-def redExpires():
+def redExpires(eventList, GREEN, time):
     # Light changes from red to green
-    return
 
-def yellowExpires():
+    # TODO: Calculate how long light will be green
+    
+    nextGreen = (time + GREEN, 'greenExpires')
+    eventList.put(nextGreen)
+
+    # End walk
+    walkEnd = (time, 'endWalk')
+    eventList.put(walkEnd)
+    return eventList
+
+def yellowExpires(eventList, RED, time):
     # Light changes from yellow to red
-    return
+    nextRed = (time+RED, 'redExpires')
+    eventList.put(nextRed)
 
-def greenExpires():
+    # Initiate start walk
+    walkStart = (time, 'startWalk')
+    eventList.put(walkStart)
+    return eventList
+
+def greenExpires(eventList, YELLOW, time):
     # Light changes from green to yellow
-    return
+    nextYellow = (time+YELLOW, 'yellowExpires')
+    eventList.put(nextYellow)
+    return eventList
 
-def startWalk():
+def startWalk(time, pedsWaiting, eventList):
     # Crosswalk sign turns on, peds start to cross
-    return
+    crossed = 0
+    i = 0
+    while i < len(pedsWaiting) and crossed < 20:
+        ped = pedsWaiting[i]
+        crosstime = 46/ped[1]
+        if crosstime < 18:
+            exitTime = time + crosstime
+            exitEvent = (exitTime, 'pedExit', ped)
+            eventList.put(exitEvent)
+            pedsWaiting.pop(i)
+            crossed += 1
+        else:
+            i += 1
+    return pedsWaiting, eventList
 
-def endWalk():
+def endWalk(pedsInSystem):
     # Crosswalk sign turns off, no more crossing
     return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
